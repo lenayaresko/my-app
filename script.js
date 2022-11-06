98cef6b5a98264acf14f0624439343c66900ddd5
Lena Yaresko <yaresko95@mail.ru>
2022-11-06 11:50:40 +0300

    layout

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const login = document.querySelector('.login-input');
-const password = document.querySelector('.password-input');
-const button = document.querySelector('.button');
-const success = document.querySelector('.content-success');
+// const login = document.querySelector('.login-input');
+// const password = document.querySelector('.password-input');
+// const button = document.querySelector('.button');
+// const success = document.querySelector('.content-success');
 
-const content = document.querySelector('.content');
+// const content = document.querySelector('.content');
 
 
-button.addEventListener("click", function (event) {
-    if (login.value !== '' && password.value !== '') {
-        request({
-            onSuccess: (data) => {
-                content.style.display = 'none';
-                success.style.display = 'flex';
-            },
-            onError: () => {
-                content.textContent = 'Ошибка';
-            }
-        })
-    } else {
-        login.classList.add('error');
-        password.classList.add('error');
+// button.addEventListener("click", function (event) {
+//     if (login.value !== '' && password.value !== '') {
+//         request({
+//             onSuccess: (data) => {
+//                 content.style.display = 'none';
+//                 success.style.display = 'flex';
+//             },
+//             onError: () => {
+//                 content.textContent = 'Ошибка';
+//             }
+//         })
+//     } else {
+//         login.classList.add('error');
+//         password.classList.add('error');
 
-    }
-});
+//     }
+// });
 
