7bccf871792df0af3233df568f34196f203fed8f
Lena Yaresko <yaresko95@mail.ru>
2022-11-07 17:41:08 +0300

    layout 2

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-const chooseLevelParent = document.querySelector('.choose-level-number-box');
 const chooseLevelStage = document.querySelectorAll('.choose-level-number');
 const chooseLevelbutton = document.querySelector('.choose-level-button');
+const chosenLevelNumberParagraph = document.querySelector('.chosen-level-number');
 
 let chosenLevelNumber;
 
-chooseLevelParent.addEventListener('click', function (event) {
-    console.log(event.target);
+
+chooseLevelStage.forEach(element => {
+    element.addEventListener("click", function (event) {
+        document.querySelectorAll('.chosenLevel').forEach(element => {
+            element.classList.remove('chosenLevel');
+        });
+        chosenLevelNumber = element.textContent;
+        element.classList.toggle('chosenLevel');
+    });
 });
 
-// chooseLevelStage.forEach(element => {
-//     element.addEventListener("click", function (event) {
-//         console.log(chooseLevelParent.children);
-//         // for (let i = 0; i < chooseLevelParent.children.length; i++) {
-//         //     element.classList.remove('chosenLevel')
-//         // }
-//         chosenLevelNumber = element.textContent;
-//         element.classList.toggle('chosenLevel');
-//     });
-// });
 
-
-chooseLevelbutton.addEventListener('click', function (event) {
-    console.log(chosenLevelNumber);
-    window.location.href = './game.html';
-
+chooseLevelbutton.addEventListener("click", function (event) {
+    document.location.href = "./game.html";
+    chosenLevelNumberParagraph.innerHTML = chosenLevelNumber;
 });
