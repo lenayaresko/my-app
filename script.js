c9f29b6b4780807e0443cf6c3d61da807f7b64af
Lena Yaresko <yaresko95@mail.ru>
2022-11-07 10:26:46 +0300

    layout

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
+const chooseLevelParent = document.querySelector('.choose-level-number-box');
 const chooseLevelStage = document.querySelectorAll('.choose-level-number');
 const chooseLevelbutton = document.querySelector('.choose-level-button');
 
-let chosenLevel;
+let chosenLevelNumber;
 
 
-chooseLevelStage.forEach(element => {
-    element.addEventListener("click", function (event) {
-        chosenLevel = element.textContent;
-        element.classList.toggle('chosenLevel');
-    });
+chooseLevelParent.addEventListener('click', function (event) {
+    console.log(event.target);
+
 });
 
+// chooseLevelStage.forEach(element => {
+//     element.addEventListener("click", function (event) {
+//         console.log(chooseLevelParent.children);
+//         // for (let i = 0; i < chooseLevelParent.children.length; i++) {
+//         //     element.classList.remove('chosenLevel')
+//         // }
+//         chosenLevelNumber = element.textContent;
+//         element.classList.toggle('chosenLevel');
+//     });
+// });
+
+
 chooseLevelbutton.addEventListener('click', function (event) {
+    console.log(chosenLevelNumber);
     window.location.href = './game.html';
+
 });
