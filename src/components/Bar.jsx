71906cfd10f26c3f33a1e595a9c516c240844985
Lena Yaresko <yaresko95@mail.ru>
2023-01-31 10:07:16 +0300

    10:07

---
+++ src/components/Bar.jsx
@@ -0,0 +1,11 @@
+function Bar() {
+return (
+<div className="centerblock__search search">
+<svg className="search__svg">
+   <use xlinkHref="img/icon/sprite.svg#icon-search" />
+</svg>
+   <input className="search__text" type="search" placeholder="Поиск" name="search"/>
+</div>)
+}
+
+export default Bar;
---
+++ src/components/Player.jsx
@@ -0,0 +1,5 @@
+function Bar() {
+return <div>Это наш bar</div>
+}
+
+export default Bar;
