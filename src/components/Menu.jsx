8347316c1ab581519629899727b6f83f70e582ab
Lena Yaresko <yaresko95@mail.ru>
2023-03-20 13:37:18 +0300

    13:37

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,19 @@
+import { useState } from 'react'
+
+const [isVisible, setVisible] = useState(false);
+
 function Menu() {
     return (
         <nav className="main__nav nav">
         <div className="nav__logo logo">
             <img className="logo__image" src="./img/logo.png" alt="logo"/>
         </div>
-        <div className="nav__burger burger">
+        <div className="nav__burger burger" onClick={ () => setVisible(!isVisible) }>
             <span className="burger__line"/>
             <span className="burger__line"/>
             <span className="burger__line"/>
         </div>
+        { isVisible && (
         <div className="nav__menu menu">
             <ul className="menu__list">
                 <li className="menu__item"><a href="http://" className="menu__link">Главное</a></li>
@@ -16,6 +21,7 @@ function Menu() {
                 <li className="menu__item"><a href="http://" className="menu__link">Войти</a></li>
             </ul>
         </div>
+        ) }
     </nav>
     )
     }
