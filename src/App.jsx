680c2e56385cfa1652b3da45381acf9f6ecf868f
Lena Yaresko <yaresko95@mail.ru>
2023-03-21 12:44:48 +0300

    12:44

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
+import { useState } from 'react';
 import Bar from "./components/Bar";
 import Menu from "./components/Menu";
 import Name from "./components/Name";
+import LoadingName from "./components/LoadingName";
+import LoadingPlayer from "./components/LoadingPlayer";
 import Player from "./components/Player";
 import './App.css';
 
 
+
 function App() {
+  const [isLoading, setLoading] = useState(true);
+
+setTimeout(() => setLoading(false), 5000);
   return (
     <div id='root' className='container'>
       <main className="main">
         <Menu />
         <Bar />
-        <Name />
+        { isLoading ? (<LoadingName />) : (<Name />)}
         </main>
         <div className="bar">
-        <Player />
+        { isLoading ? (<LoadingPlayer />) : (<Player />)}
         </div>
     </div>
 );
