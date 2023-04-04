9892a273a2c86573603968036bb948cec316ad56
Lena Yaresko <yaresko95@mail.ru>
2023-04-04 15:44:43 +0300

    hw 5.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import { useState } from 'react';
-import Bar from "./components/Bar";
-import Menu from "./components/Menu";
-import Name from "./components/Name";
-import LoadingName from "./components/LoadingName";
-import LoadingPlayer from "./components/LoadingPlayer";
-import Player from "./components/Player";
-import './App.css';
-
-
+import { useEffect, useState } from 'react'
+import Bar from './components/Bar'
+import Menu from './components/Menu'
+import Name from './components/Name'
+import LoadingName from './components/LoadingName'
+import LoadingPlayer from './components/LoadingPlayer'
+import Player from './components/Player'
+import './App.css'
 
 function App() {
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 5000)
+  }, [])
 
-setTimeout(() => setLoading(false), 5000);
   return (
-    <div id='root' className='container'>
+    <div id="root" className="container">
       <main className="main">
         <Menu />
         <Bar />
-        { isLoading ? (<LoadingName />) : (<Name />)}
-        </main>
-        <div className="bar">
-        { isLoading ? (<LoadingPlayer />) : (<Player />)}
-        </div>
+        {isLoading ? <LoadingName /> : <Name />}
+      </main>
+      <div className="bar">{isLoading ? <LoadingPlayer /> : <Player />}</div>
     </div>
-);
+  )
 }
 
-export default App;
+export default App
